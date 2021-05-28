d34f989cece5619d7f9cfa00751234192d409c74
Rodrigo Vieira <rodvieirasilva@gmail.com>
2021-05-28 09:23:18 -0300

    Add base project

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Stack, Text, Link, FontWeights, IStackTokens } from '@fluentui/react';
+import './App.css';
+import BingMap from './BingMap';
+
+const boldStyle = { root: { fontWeight: FontWeights.semibold } };
+const stackTokens: IStackTokens = { childrenGap: 15 };
+
+export const App: React.FunctionComponent = () => {
+  return (
+    <Stack
+      horizontalAlign="center"
+      verticalAlign="center"
+      verticalFill
+      styles={{
+        root: {
+          width: '100%',
+          margin: '0 auto',
+          textAlign: 'center',
+          color: '#605e5c',
+        },
+      }}
+      tokens={stackTokens}
+    >
+     <BingMap
+      mapOptions={{
+        center: [47.60357, -122.32945]
+      }}
+    />
+    </Stack>
+  );
+};
